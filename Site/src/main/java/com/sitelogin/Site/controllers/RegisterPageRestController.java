1e0a288f4b0afbc7d0578ceef5ee4dad4f20c8a6
MadalinaIstrate <istrate_madalina7@yhoo.com>
2019-05-03 13:52:42 +0300

    Save booking from another app

--- Site/src/main/java/com/sitelogin/Site/controllers/RegisterPageRestController.java
+++ Site/src/main/java/com/sitelogin/Site/controllers/RegisterPageRestController.java
@@ -1,9 +1,13 @@
 package com.sitelogin.Site.controllers;
 
+import com.sitelogin.Site.domain.Booking;
 import com.sitelogin.Site.domain.User;
+import com.sitelogin.Site.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,6 +16,8 @@ public class RegisterPageRestController {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private BookingService bookingService;
 
     @GetMapping("/get-register/{id}")
     public ModelAndView register(@PathVariable("id") int id) {
@@ -24,16 +30,43 @@ public class RegisterPageRestController {
         return mav;
     }
 
+    @GetMapping("/save-booking/{userId}/{bookId}")
+    public void saveBooking1(@PathVariable("userId") int userId,
+                            @PathVariable("bookId") int bookId){
+        Booking booking = restTemplate.getForObject("http://localhost:8090/save-booking/{userId}/{bookId}", Booking.class, userId, bookId);
+        try{
+            bookingService.addBooking(booking);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/save-booking")
+    public void saveBooking2(@RequestParam("userId") int userId,
+                             @RequestParam("bookId") int bookId){
+        Booking booking = restTemplate.getForObject("http://localhost:8090/save-booking", Booking.class, userId, bookId);
+        try{
+            bookingService.addBooking(booking);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @PostMapping("/save-user")
     public void saveUser(){
-        //User user = new User("lastName", "firstName", 100, "userName", "parolaaaaa", "lastname@yahoo.com");
+        //User user = new User("lastName", "firstName", 100, "userName", "pas", "lastname@yahoo.com");
         User user = new User();
-        user.setEmail("emailnou@gmail.com");
-        user.setLastName("email1");
-        user.setFirstName("email1");
-        user.setPassword("email1");
-        user.setUserName("email1");
-        restTemplate.postForObject("http://localhost:8090/save-user", user,  User.class);
+        user.setEmail("em12@gmail.com");
+        user.setLastName("emafde");
+        user.setFirstName("emaiewe");
+        user.setPassword("passsss");
+        user.setUserName("ema");
+        try{
+            restTemplate.postForObject("http://localhost:8090/save-user", user,  User.class);
+        }
+        catch(HttpClientErrorException e){
+            e.printStackTrace();
+        }
     }
 
    /* @PostMapping("saving-failed")
@@ -41,9 +74,6 @@ public class RegisterPageRestController {
         return message;
 
     } */
-    @PostMapping("saving-failed")
-    public ResponseEntity savingFailed(@RequestBody ResponseEntity responseEntity){
-        return responseEntity;
-    }
+
 
 }
